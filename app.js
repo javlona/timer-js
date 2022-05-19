23cd8b6b344c1f9aaefd5b05afe84ebec7e0e559
Javlon <59303287+javlona@users.noreply.github.com>
2022-05-18 22:54:36 -0500

    fixed bugs

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 const startTimer = document.querySelector('#startTimer')
 const pauseTimer = document.querySelector('#pauseTimer')
+const cancelTimer = document.querySelector('#cancelTimer')
 let displayTimer = document.querySelector('#displayTimer')
 let [second, minute, hour] = [0, 0, 0]
 let start = null
 
 startTimer.addEventListener('click', () => {
-    if(startTimer.innerText === 'Start'){
-        startTimer.innerText = 'Stop'
-        startTimer.classList.add('btn-danger')
-    } else {
-        startTimer.innerText = 'Start'
-        startTimer.classList.remove('btn-danger')
-        // clearInterval(start)
-    }
+    // if(startTimer.innerText === 'Start'){
+    //     startTimer.innerText = 'Stop'
+    //     startTimer.classList.add('btn-danger')
+    // } else {
+    //     startTimer.innerText = 'Start'
+    //     startTimer.classList.remove('btn-danger')
+    //     // clearInterval(start)
+    // }
     if(start!==null){
         clearInterval(start)
       }
@@ -21,6 +22,15 @@ startTimer.addEventListener('click', () => {
 
 pauseTimer.addEventListener('click', () => {
     clearInterval(start)
+})
+
+cancelTimer.addEventListener('click', () => {
+    clearInterval(start)
+    displayTimer.innerText = '00h:00m:00s'
+    second = 0
+    minute = 0
+    hour = 0
+    console.log(second, minute, hour)
 })
 
 function timer() {
